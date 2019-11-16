c518e578a5458fa210014042af64fab30c2acc75
Nilton Lopes <niltonrck@gmail.com>
2019-11-16 17:45:08 -0300

    INT: Adiciona front-end das contas

--- src/components/Widgets/BillsWidget/index.js
+++ src/components/Widgets/BillsWidget/index.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoneyBill, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 import DefaultWidget from "../DefaultWidget";
+import Check from "../../Buttons/Check";
 
 export default function BillsWidget() {
   const [bills] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -16,14 +17,14 @@ export default function BillsWidget() {
             <li className="bill">
               <div className="bill-date">
                 <span className="bill-day">10/11</span>
-                <span className="bill-date-week">Friday</span>
+                <span className="bill-day-week">Friday</span>
               </div>
-              <span className="bill-type">Iconme</span>
+              <span className="bill-type">Income</span>
               <span className="bill-value">1300,00</span>
               <span className="bill-name">Aluguel</span>
               <div className="bill-acions">
                 <span className="bill-paid">
-                  <FontAwesomeIcon icon={faCheck} />
+                  <Check />
                 </span>
               </div>
             </li>
