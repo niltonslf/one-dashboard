f9a9d8d63cb3e247ee87d71cc8839375bee930d1
Nilton Lopes <niltonrck@gmail.com>
2019-11-17 00:59:14 -0300

    Remoção de uma lib process ¬¬

--- src/services/BillService.js
+++ src/services/BillService.js
@@ -1,18 +1,17 @@
 import axios from "axios";
-import { process } from "ipaddr.js";
 
 console.log(process.env);
 
 class BillService {
   constructor() {
     this.key = process.env.REACT_APP_ORGANIZZE_KEY;
-    this.apiUrl = ``;
+    this.apiUrl = "https://api.organizze.com.br/rest/v2/";
     this.user = process.env.REACT_APP_ORGANIZZE_USERMAIL;
 
     this.authorization = btoa(`${this.user}:${this.key}`);
 
     this.axios = axios.create({
-      baseURL: "https://api.organizze.com.br/rest/v2/",
+      baseURL: this.apiUrl,
       headers: {
         "Access-Control-Allow-Origin": "*",
         Authorization: `Basic ${this.authorization}`,
