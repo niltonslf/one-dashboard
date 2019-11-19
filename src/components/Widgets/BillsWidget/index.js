87f9ed7f75cc58f5e81316307e9fb057fe9ece74
Nilton Lopes <niltonrck@gmail.com>
2019-11-19 00:39:52 -0300

    WIP: retorna dados da api

--- src/components/Widgets/BillsWidget/index.js
+++ src/components/Widgets/BillsWidget/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
 
+import moment from "moment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 
@@ -14,7 +15,7 @@ export default function BillsWidget() {
 
   useEffect(() => {
     BillService.fetchBills().then(bills => {
-      setBills(bills)
+      setBills(bills);
     });
   }, []);
 
@@ -25,13 +26,15 @@ export default function BillsWidget() {
           return (
             <li className="bill" key={index}>
               <div className="bill-date">
-                <span className="bill-day">{bill.date}</span>
-                <span className="bill-day-week">Friday</span>
+                <span className="bill-day">
+                  {moment(bill.date).format("D/M/Y")}
+                </span>
+                <span className="bill-day-week">{bill.weekday}</span>
               </div>
               <span className="bill-type">
-                <span className="tag">Income</span>
+                <span className="tag">{bill.type}</span>
               </span>
-              <span className="bill-value">{bill.amount_cents}</span>
+              <span className="bill-value">R$ {bill.price}</span>
               <span className="bill-name">{bill.description}</span>
               <div className="bill-acions">
                 <span className="bill-paid">
